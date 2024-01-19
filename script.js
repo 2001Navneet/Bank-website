eab59ed48aa8753af357c0c89fb6910569d42faa
Navneet <navneetverma699@gmail.com>
2024-01-19 16:33:28 +0530

    tabed feature

--- script.js
+++ script.js
@@ -57,3 +57,22 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 //Tabbed Component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+  // Ignore the click where the result is null
+  if (!clicked) return; // gaurd class
+  //remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // activate content area
+  clicked.classList.add('operations__tab--active');
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
